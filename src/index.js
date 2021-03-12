c951c881c71f63273b9532411795813e4e4c2fc0
catfoxb <catfoxb@gmail.com>
2021-03-12 23:32:35 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 === 1) return false;
+
+    let map = new Map(bracketsConfig);
+    let resultArray = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === map.get(resultArray[resultArray.length - 1])) {
+            resultArray.pop();
+        } else {
+            resultArray.push(str[i]);
+        }
+    }
+
+    return resultArray.length === 0;
+};
